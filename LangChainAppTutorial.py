d1592c6e8fed31fb837d44de34bc1f19c466bbba
Vyshnav V G <vyshnavvg@gmail.com>
2023-12-31 00:03:50 +0530

    add pdf and split text into chunks

--- LangChainAppTutorial.py
+++ LangChainAppTutorial.py
@@ -1,1 +1,43 @@
-print("Hello World");
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap =200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title ="Virtual Assitant For Field Engineers", page_icon=":books:")
+
+    st.header("Chat with Me :books:")
+    st.text_input("Ask a question: ")
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDF and click on process", accept_multiple_files=True)
+        if st.button("process") :
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
+                # create vector store
+
+if __name__ == '__main__':
+    main()
